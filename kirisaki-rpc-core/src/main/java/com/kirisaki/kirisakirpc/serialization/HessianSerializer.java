3ddc74a97b83fbd942ae90d44f7c0a9a5b931aca
kirisaki <735647137@qq.com>
2024-03-30 19:19:36 +0800

    引入多种序列化器

---
+++ kirisaki-rpc-core/src/main/java/com/kirisaki/kirisakirpc/serialization/HessianSerializer.java
@@ -0,0 +1,28 @@
+package com.kirisaki.kirisakirpc.serialization;
+
+import com.caucho.hessian.io.HessianInput;
+import com.caucho.hessian.io.HessianOutput;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+/**
+ * Hessian序列化器
+ */
+public class HessianSerializer implements Serializer {
+    @Override
+    public <T> byte[] serialize(T object) throws IOException {
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        HessianOutput ho = new HessianOutput(bos);
+        ho.writeObject(bos);
+        return bos.toByteArray();
+    }
+
+    @Override
+    public <T> T deserialize(byte[] bytes, Class<T> tClass) throws IOException {
+        ByteArrayInputStream bis = new ByteArrayInputStream(bytes);
+        HessianInput hi = new HessianInput(bis);
+        return (T) hi.readObject();
+    }
+}
---
+++ kirisaki-rpc-core/src/main/java/com/kirisaki/kirisakirpc/serialization/KryoSerializer.java
@@ -0,0 +1,37 @@
+package com.kirisaki.kirisakirpc.serialization;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+public class KryoSerializer implements Serializer {
+    //todo lanmda 表达式要用 -> 而不是 =>
+    private static final ThreadLocal<Kryo> KRYO_THREAD_LOCAL = ThreadLocal.withInitial(() -> {
+        Kryo kryo = new Kryo();
+        //设置动态序列化和反序列化类,不提前注册所有类(可能有安全问题)
+        kryo.setRegistrationRequired(false);
+        return kryo;
+    });
+
+    @Override
+    public <T> byte[] serialize(T obj) throws IOException {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        Output output = new Output(byteArrayOutputStream);
+        KRYO_THREAD_LOCAL.get().writeObject(output, obj);
+        output.close();
+        return byteArrayOutputStream.toByteArray();
+    }
+
+    @Override
+    public <T> T deserialize(byte[] bytes, Class<T> classType) throws IOException {
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes);
+        Input input = new Input(byteArrayInputStream);
+        T result = KRYO_THREAD_LOCAL.get().readObject(input, classType);
+        input.close();
+        return result;
+    }
+}
